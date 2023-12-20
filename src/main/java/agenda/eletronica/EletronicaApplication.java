7aef9c2da381fb04599a7141c8422dd1e33ddd73
Felipe Lúcio <felipesou94@gmail.com>
2023-12-20 14:18:02 -0300

    Create project e config properties

---
+++ src/main/java/agenda/eletronica/EletronicaApplication.java
@@ -0,0 +1,13 @@
+package agenda.eletronica;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class EletronicaApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(EletronicaApplication.class, args);
+	}
+
+}
